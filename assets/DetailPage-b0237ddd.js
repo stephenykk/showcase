3bea7f1646bcd85dee79ae3728fcaa8e5e9b54d1
stephenykk <stephenykk@163.com>
2023-09-25 03:26:09 +0800

    update rebuild files

--- assets/DetailPage-b0237ddd.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-d3a439bc.js";function a(){return e.jsx("div",{children:"DetailPage page"})}export{a as default};
---
+++ assets/DetailPage-d7f19f2f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e0916e6c.js";function a(){return e.jsx("div",{children:"DetailPage page"})}export{a as default};
