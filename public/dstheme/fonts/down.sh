d31e287f0a860ee97f182023143c326647b1defd
stephenykk <stephenykk@163.com>
2023-09-26 08:47:12 +0800

    remove public dir

--- public/dstheme/fonts/down.sh
+++
@@ -1,9 +0,0 @@
-# url=$1
-url="$*"
-fname=${url##*/}
-sname=${fname%%\?*}
-name=${sname%%\#*}
-
-# echo $name !!!
-
-curl -o "$name" "$url"
--- public/dstheme/fonts/startDown.sh
+++
@@ -1,1 +0,0 @@
-cat url2.txt | xargs -t -d"\n" -i bash down.sh {} 
